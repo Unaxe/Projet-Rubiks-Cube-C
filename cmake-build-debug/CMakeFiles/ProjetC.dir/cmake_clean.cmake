9f3910e8ef2867919a69a80b6149d8ad44aac64f
Unaxe <74629449+Unaxe@users.noreply.github.com>
2021-04-19 17:18:17 +0200

    Init main.c

---
+++ cmake-build-debug/CMakeFiles/ProjetC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetC.dir/main.c.obj"
+  "ProjetC.exe"
+  "ProjetC.exe.manifest"
+  "ProjetC.pdb"
+  "libProjetC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
